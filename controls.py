a8bdfe0928976dfb38c4228b98a7900b9c2c9f61
ErikRospo <87732519+ErikRospo@users.noreply.github.com>
2025-06-24 10:45:04 -0700

    Typing controls

--- controls.py
+++ controls.py
@@ -1,13 +1,18 @@
+from typing import Literal, assert_never
+
+
 class Controls:
     def __init__(self):
         self.lmb_down = False
         self.rmb_down = False
         self.mmb_down = False
 
-    def set_mb(self, button: int, state=True):
+    def set_mb(self, button:Literal[0,1,2], state=True):
         if button == 0:
             self.lmb_down = state
         elif button == 1:
             self.rmb_down = state
         elif button == 2:
             self.mmb_down = state
+        else:
+            assert_never(button)
